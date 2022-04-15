61ed4e100aaff927bf42662dbbfd3fe8e6cb9e4b
Dan Paslawsky <danpaslawsky@gmail.com>
2022-04-15 14:58:09 -0700

    commit tree 3836193d6590d69398ef1ae1448017a44878d3d2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,19 @@ RSpec.configure do |config|
   config.order = 'default'
 end
 
+# Load RSpec and Capybara
+require 'rspec'
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+# Configure RSpec
+RSpec.configure do |config|
+  # Mixin the Capybara functionality into Rspec
+  config.include Capybara::DSL
+  config.order = 'default'
+end
+
+
 # Define the application we're testing
 def app
   # Load the application defined in config.ru
